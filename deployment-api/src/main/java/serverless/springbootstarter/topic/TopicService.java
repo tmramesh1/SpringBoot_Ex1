a151e29cb458692abd38dc63d3e6779034394e67
Test <thinakaran.k@hotmail.com>
2017-07-17 09:37:12 +0530

    update

--- deployment-api/src/main/java/serverless/springbootstarter/topic/TopicService.java
+++ deployment-api/src/main/java/serverless/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package serverless.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private  List<Topic> topics = Arrays.asList(
+	private  List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Spring", "Spring framework", "Spring Framework Descripion"),
 			new Topic("java", "Core Java", "Core Java Description"),
 			new Topic("javascript", "JavaScript", "JavaScript Description")
-			);
+			));
 	
 	public List<Topic>  getAllTopics(){
 		return topics;
@@ -25,6 +26,22 @@ public class TopicService {
 
 	public void addTopic(Topic topic) {
 		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i = 0; i < topics.size(); i++)
+		{
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)){
+				topics.set(i,  topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t->t.getId().equals(id));
 	}
 }
 
